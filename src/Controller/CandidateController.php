cac2ffc53c215b2573c75cf3e9fcf884aa4d4f2e
lemaj42 <abdelmadjidchabane89@gmail.com>
2024-03-20 14:23:16 +0100

    r

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -4,78 +4,42 @@ namespace App\Controller;
 
 use App\Entity\Candidate;
 use App\Form\CandidateType;
-use App\Repository\CandidateRepository;
+use App\Repository\CandidatsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
-#[Route('/candidate')]
+#[Route('/candidats')]
 class CandidateController extends AbstractController
 {
-    #[Route('/', name: 'app_candidate_index', methods: ['GET'])]
-    public function index(CandidateRepository $candidateRepository): Response
-    {
-        return $this->render('candidate/index.html.twig', [
-            'candidates' => $candidateRepository->findAll(),
-        ]);
-    }
 
-    #[Route('/new', name: 'app_candidate_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, UserInterface $user): Response
+    #[Route('/edit', name: 'app_candidate_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
-        $candidate = new Candidate();
-        $candidate->setUser($user);
-        $form = $this->createForm(CandidateType::class, $candidate);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($candidate);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_candidate_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('candidate/new.html.twig', [
-            'candidate' => $candidate,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_candidate_show', methods: ['GET'])]
-    public function show(Candidate $candidate): Response
-    {
-        return $this->render('candidate/show.html.twig', [
-            'candidate' => $candidate,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_candidate_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Candidate $candidate, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CandidateType::class, $candidate);
+        $candidat = $user->setCandidate();
+        $form = $this->createForm(CandidateType::class, );
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_candidate_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_candidate_edit', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('candidate/edit.html.twig', [
-            'candidate' => $candidate,
+            'candidat' => $candidat,
             'form' => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'app_candidate_delete', methods: ['POST'])]
-    public function delete(Request $request, Candidate $candidate, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Candidate $candidat, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $candidate->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($candidate);
+        if ($this->isCsrfTokenValid('delete' . $candidat->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($candidat);
             $entityManager->flush();
         }
 
